791b26818c692233f407f6347c5925c8b34ab2b7
birbchurch <b11002078@g.chu.edu.tw>
2025-01-31 10:03:57 +0800

    Add Edit Button

--- frontend/birby-hr-web/src/pages/staffs/view/StaffsView.tsx
+++ frontend/birby-hr-web/src/pages/staffs/view/StaffsView.tsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import StaffsViewLoader from "../../../routes/loaders/StaffsViewLoader";
 import Container from "../../../components/common/general/Container";
 import TitleHeader from "../../../components/common/general/TitleHeader";
@@ -9,6 +9,13 @@ function StaffsView() {
         <>
             <Container>
                 <TitleHeader title="Staff Management / Staff" />
+                <div className="flex flex-wrap w-full mb-3">
+                    <Link to={"/staffs/edit/1234"}>
+                        <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Edit
+                        </button>
+                    </Link>
+                </div>
                 <div className="flex flex-wrap w-full justify-center">
                     <img src="https://fakeimg.pl/500x500/" className="size-60 mb-5" />
                     <div className="grow">
@@ -41,10 +48,10 @@ function StaffsView() {
                                     </span>
                                 </h4>
                             </div>
-                            <div className="text-2xl w-sm bg-amber-100 h-full rounded-2xl">
+                            <div className="text-2xl w-sm h-full rounded-2xl bg-amber-100 overflow-hidden">
                                 <table className="w-full text-center">
                                     <thead>
-                                        <tr className="border-b-2 bg-amber-200 border-amber-400">
+                                        <tr className="border-b-2 border-amber-400 rounded-t-2xl bg-amber-300">
                                             <th className="p-2">
                                                 Roles
                                             </th>
