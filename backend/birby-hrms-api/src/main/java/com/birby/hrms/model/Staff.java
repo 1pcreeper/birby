46ffc091aa7fdde3525102ca8eb36be258671812
1pcreeper <linchikin116@gmail.com>
2025-02-11 20:55:30 +0800

    new file:   backend/birby-hrms-api/src/main/java/com/birby/hrms/model/Staff.java 	modified:   docker/src/database/hrms_db/sql/1.sql 	new file:   template/test/sql/ER3.png

---
+++ backend/birby-hrms-api/src/main/java/com/birby/hrms/model/Staff.java
@@ -0,0 +1,23 @@
+package com.birby.hrms.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "staff")
+public class Staff {
+    @Id
+    @Column(name = "id", nullable = false)
+    private String id;
+    
+}
--- docker/src/database/hrms_db/sql/1.sql
+++ docker/src/database/hrms_db/sql/1.sql
@@ -1,7 +1,22 @@
 CREATE TABLE staff(
     id VARCHAR(255) NOT NULL,
-    gender BOOLEAN NOT NULL,
     PRIMARY KEY (id)
+);
+CREATE TABLE job_type(
+    id VARCHAR(255) NOT NULL,
+    name VARCHAR(20) NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE TABLE staff_detail(
+	staff_id VARCHAR(255) NOT NULL,
+    display_name VARCHAR(20) NOT NULL,
+    gender BOOLEAN NOT NULL,
+    birth DATE NULL,
+    hk_phone VARCHAR(8) UNIQUE CHECK (REGEXP_LIKE(hk_phone, '^[0-9]{8}$')),
+    job_type_id VARCHAR(255) NOT NULL,
+    PRIMARY KEY (staff_id),
+    FOREIGN KEY (staff_id) REFERENCES staff(id),
+    FOREIGN KEY (job_type_id) REFERENCES job_type(id)
 );
 
 CREATE TABLE leave_approval(
