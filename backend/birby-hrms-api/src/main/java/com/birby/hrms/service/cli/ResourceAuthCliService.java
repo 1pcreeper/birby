07edf65f3fac67ff4792de95984ac4459cfbbb73
1pcreeper <linchikin116@gmail.com>
2025-02-15 01:56:16 +0800

    deleted:    backend/birby-hrms-api/src/main/java/com/birby/hrms/filter/JwtRequestFilter.java 	deleted:    backend/birby-hrms-api/src/main/java/com/birby/hrms/mapper/StaffMapper.java 	deleted:    backend/birby-hrms-api/src/main/java/com/birby/hrms/mapper/impl/StaffMapperImpl.java 	deleted:    backend/birby-hrms-api/src/main/java/com/birby/hrms/service/cli/ResourceAuthCliService.java 	deleted:    backend/birby-hrms-api/src/main/java/com/birby/hrms/service/cli/ResourceRegisterCliService.java 	deleted:    backend/birby-hrms-api/src/main/java/com/birby/hrms/service/cli/impl/ResourceAuthCliServiceImpl.java 	deleted:    backend/birby-hrms-api/src/main/java/com/birby/hrms/service/cli/impl/ResourceRegisterCliServiceImpl.java 	deleted:    backend/birby-hrms-api/src/main/java/com/birby/hrms/service/control/StaffControlService.java 	deleted:    backend/birby-hrms-api/src/main/java/com/birby/hrms/service/control/impl/StaffControlServiceImpl.java 	deleted:    backend/birby-hrms-api/src/main/java/com/birby/hrms/vo/JwtPrincipal.java
     Changes not staged for commit:
    	modified:   backend/birby-hrms-account-api/pom.xml
    	modified:   backend/birby-hrms-account-api/src/main/java/com/birby/hrms_account_api/controller/StaffRoleController.java
    	modified:   backend/birby-hrms-account-api/src/main/java/com/birby/hrms_account_api/service/manager/StaffRoleManagerService.java
    	modified:   backend/birby-hrms-account-api/src/main/java/com/birby/hrms_account_api/service/manager/impl/StaffRoleManagerServiceImpl.java
    	modified:   backend/birby-hrms-api/pom.xml
    	deleted:    backend/birby-hrms-api/src/main/java/com/birby/hrms/HrmsApplication.java
    	deleted:    backend/birby-hrms-api/src/main/java/com/birby/hrms/client/ResourceRegisterClient.java
    	deleted:    backend/birby-hrms-api/src/main/java/com/birby/hrms/controller/StaffController.java
    	modified:   backend/birby-hrms-api/src/main/java/com/birby/hrms/controller/TestController.java
    	deleted:    backend/birby-hrms-api/src/main/java/com/birby/hrms/dto/ApiResponse.java
    	deleted:    backend/birby-hrms-api/src/main/java/com/birby/hrms/dto/request/StaffCreateReqDto.java
    	deleted:    backend/birby-hrms-api/src/main/java/com/birby/hrms/dto/response/StaffCreateResDto.java
    	modified:   backend/birby-hrms-api/src/main/java/com/birby/hrms/properties/ClientProperties.java
    	modified:   backend/birby-hrms-api/src/main/java/com/birby/hrms/properties/SecurityProperties.java
    	modified:   backend/birby-hrms-api/src/main/resources/.env
    	modified:   backend/birby-hrms-api/src/main/resources/.env.demo
    	deleted:    backend/birby-hrms-api/src/test/java/com/birby/hrms/HrmsApplicationTests.java
    
     Untracked files:
    	backend/birby-hrms-account-api/src/main/java/com/birby/hrms_account_api/dto/response/StaffRoleIdsResDto.java
    	backend/birby-hrms-account-api/src/main/java/com/birby/hrms_account_api/mapper/StaffRoleMapper.java
    	backend/birby-hrms-account-api/src/main/java/com/birby/hrms_account_api/mapper/impl/StaffRoleMapperImpl.java
    	backend/birby-hrms-account-api/src/main/java/org/
    	backend/birby-hrms-api/src/main/java/com/birby/hrms/HRMSApiApplication.java
    	backend/birby-hrms-api/src/test/java/com/birby/hrms/HRMSApiApplicationTests.java

--- backend/birby-hrms-api/src/main/java/com/birby/hrms/service/cli/ResourceAuthCliService.java
+++
@@ -1,7 +0,0 @@
-package com.birby.hrms.service.cli;
-
-import com.birby.hrms.bo.response.ResourceAuthResCliBo;
-
-public interface ResourceAuthCliService {
-    ResourceAuthResCliBo getAuthData(String bearer);
-}
