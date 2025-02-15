4c5b978a7dd62c55e6833185ceabd55ec920eb50
1pcreeper <linchikin116@gmail.com>
2025-02-16 05:58:01 +0800

    renamed:    frontend/birby-hr-web/src/app/firebase/FirebaseApp.tsx -> frontend/birby-hr-web/src/app/FirebaseApp.tsx 	modified:   frontend/birby-hr-web/src/main.tsx 	deleted:    frontend/birby-hr-web/src/programs/background/BackgroundProgram.tsx

--- frontend/birby-hr-web/src/main.tsx
+++ frontend/birby-hr-web/src/main.tsx
@@ -4,7 +4,6 @@ import App from './App'
 import MatrixImpl from './contexts/matrix/MatrixImpl';
 import MatrixContext from './contexts/MatrixContext';
 import Matrix from './contexts/matrix/Matrix';
-import BackgroundProgram from './programs/background/BackgroundProgram';
 
 const matrix: Matrix = new MatrixImpl();
 createRoot(document.getElementById('root')!).render(
@@ -14,4 +13,3 @@ createRoot(document.getElementById('root')!).render(
     </MatrixContext.Provider>
   </StrictMode>
 );
-BackgroundProgram.run(matrix);
