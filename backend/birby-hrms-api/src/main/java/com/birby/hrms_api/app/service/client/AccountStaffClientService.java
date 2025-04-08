c4e9c8f45a41512915e97f62de7b75276889e14c
LinChiKin <linchikin116@gmail.com>
2025-04-08 12:33:40 +0800

    deleted:    backend/birby-hrms-api/src/main/java/com/birby/hrms_api/app/client/AccountStaffClient.java 	deleted:    backend/birby-hrms-api/src/main/java/com/birby/hrms_api/app/service/client/AccountStaffClientService.java 	deleted:    backend/birby-hrms-api/src/main/java/com/birby/hrms_api/app/service/client/impl/AccountStaffClientServiceImpl.java 	deleted:    backend/birby-hrms-api/src/main/java/com/birby/hrms_api/app/service/entity/StaffEntityService.java 	deleted:    backend/birby-hrms-api/src/main/java/com/birby/hrms_api/app/service/entity/impl/StaffEntityServiceImpl.java 	modified:   backend/birby-hrms-account-api/src/main/java/com/birby/hrms_account_api/app/component/properties/KafkaProperties.java 	modified:   backend/birby-hrms-account-api/src/main/java/com/birby/hrms_account_api/app/config/KafkaConsumerConfig.java 	modified:   backend/birby-hrms-account-api/src/main/resources/.env.example 	modified:   backend/birby-hrms-account-api/src/main/resources/application.yml 	modified:   backend/birby-hrms-api/src/main/java/com/birby/hrms_api/app/client/AccountStaffRoleClient.java 	modified:   backend/birby-hrms-api/src/main/java/com/birby/hrms_api/app/component/properties/KafkaProperties.java 	modified:   backend/birby-hrms-api/src/main/java/com/birby/hrms_api/app/config/KafkaConsumerConfig.java 	modified:   backend/birby-hrms-api/src/main/java/com/birby/hrms_api/app/model/response/ApiResponse.java 	modified:   backend/birby-hrms-api/src/main/java/com/birby/hrms_api/app/service/client/impl/AccountStaffRoleClientServiceImpl.java 	modified:   backend/birby-hrms-api/src/main/resources/.env 	modified:   backend/birby-hrms-api/src/main/resources/.env.example 	modified:   backend/birby-hrms-api/src/main/resources/application.yml 	modified:   docker-compose.yml

--- backend/birby-hrms-api/src/main/java/com/birby/hrms_api/app/service/client/AccountStaffClientService.java
+++
@@ -1,8 +0,0 @@
-package com.birby.hrms_api.app.service.client;
-
-import com.birby.hrms_api.app.model.cto.res.AccountStaffV1ResCTO;
-import com.birby.hrms_api.app.model.exception.ClientServiceException;
-
-public interface AccountStaffClientService {
-    AccountStaffV1ResCTO getStaffV1(String staffId) throws ClientServiceException;
-}
